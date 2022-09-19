6db7a182a32f3c37766b6a701043e1f4240770da
Feyruz Qemberov <you@example.com>
2022-09-20 01:03:49 +0400

    Added new html files, images and js

---
+++ slider.js
@@ -0,0 +1,36 @@
+var models = [ 
+    {
+        name : 'BMW M5 E60', 
+        img : 'images/bmw-e60.jpg',
+        link : 'https://www.bmwblog.com/2022/03/21/bmw-m5-e60-autobahn-high-speed/'
+    },
+    {
+        name : 'BMW M4 F82', 
+        img : 'images/bmw-m4.jpg',
+        link : 'https://machineswithsouls.com/2022/03/14/the-bmw-f82-m4-buyers-guide/'
+    },
+    {
+        name : 'BMW M5 F90', 
+        img : 'images/bmw-f90.jpg',
+        link : 'https://www.evo.co.uk/bmw/m5'
+    },
+    {
+        name : 'BMW M8 Competition', 
+        img : 'images/bmw-m8.jpg',
+        link : 'https://www.bmw-m.com/en/topics/magazine-article-pool/bmw-m8-bmw-m8-competition-automobile-2022.html'
+    },
+    {
+        name : 'BMW X5 M50', 
+        img : 'images/BMW-X5-00.jpg',
+        link : 'https://www.edmunds.com/bmw/x5/2021/m50i/'
+    }
+];
+
+var index = 3;
+
+
+document.querySelector('.card-title').textContent = models[index].name;
+
+document.querySelector('.card-link').setAttribute('href', models[index].link);
+
+document.querySelector('.card-img-top').setAttribute('src', models[index].img);
